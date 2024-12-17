7cd971144e96954d550c20f1cf34cc5110543594
Pedro <ph-felix@hotmail.com>
2024-12-17 00:39:08 -0300

    corrigindo erro de permissão

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://serverest.dev',
-    supportFile: false,
+    supportFile: 'cypress/support/e2e.js',
     setupNodeEvents(on, config) {
     }
   }
